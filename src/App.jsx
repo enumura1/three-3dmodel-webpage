b0d00bb685f7bd839c6b43e3de08c4fcdfe19308
enumura1 <akirayuto0718@gmail.com>
2023-11-22 22:50:28 +0900

    3dmodel animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from '@react-three/fiber'
+import { Stats, OrbitControls, useGLTF } from '@react-three/drei'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function Model() {
+  const { scene } = useGLTF("assets/moon_and_star.glb")
+  return <primitive object={scene} />
 }
 
-export default App;
+export default function App() {
+
+  return (
+    <>
+      <Canvas camera={{ position: [2, 1, 2], near: 0.05 }}>
+        <group>
+          <Model />
+        </group>
+        <OrbitControls autoRotate />
+        <Stats />
+      </Canvas>
+    </>
+  )
+}
